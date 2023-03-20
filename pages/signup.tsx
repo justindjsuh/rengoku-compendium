47298f3dd72a0aabb0f26a095f89e55e275da458
Justin Suh <justindjsuh@gmail.com>
2023-03-19 21:19:31 -0400

    creating signup page

---
+++ pages/signup.tsx
@@ -0,0 +1,11 @@
+import Head from "next/head";
+
+export default function Signup() {
+  return (
+    <>
+      <Head>
+        <title>Rengoku Compendium | Sign Up</title>
+      </Head>
+    </>
+  );
+}

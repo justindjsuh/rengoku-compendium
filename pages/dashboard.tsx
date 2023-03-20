f1ffa21bfa2a6e42ebfd4d7345f50044ce9504ed
Justin Suh <justindjsuh@gmail.com>
2023-03-20 13:19:52 -0400

    side menu component finished

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,6 +1,10 @@
 import { useUser } from "@supabase/auth-helpers-react";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
+import styles from "@/styles/Dashboard.module.css";
+import Image from "next/image";
+import Link from "next/link";
+import SideMenu from "@/comps/SideMenu";
 
 export default function Dashboard() {
   const user = useUser();
@@ -10,5 +14,10 @@ export default function Dashboard() {
     if (!user) router.push("/");
   });
 
-  return <h1>DASHBOARD</h1>;
+  return (
+    <div className={styles.container}>
+      <SideMenu />
+      <div className={styles.dashContainer}></div>
+    </div>
+  );
 }

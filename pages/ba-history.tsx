0dcaf48be0a75a9ccce358d8a7b28d182a5c187d
Justin Suh <justindjsuh@gmail.com>
2023-03-27 12:14:51 -0400

    removed random div

--- pages/ba-history.tsx
+++ pages/ba-history.tsx
@@ -89,7 +89,6 @@ export default function BAHistory() {
             ) : (
               <p>Select a date to see details</p>
             )}
-            <div></div>
           </div>
         </div>
       </div>

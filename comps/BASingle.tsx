e8e067f6b04f2f60a7cb400e232e558d6537f88a
Justin Suh <justindjsuh@gmail.com>
2023-03-27 12:06:46 -0400

    render BA for each date and formatted

--- comps/BASingle.tsx
+++ comps/BASingle.tsx
@@ -1,3 +1,7 @@
+import styles from "@/styles/BASingle.module.css";
+import { faStopwatch } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 type BA_Single = {
   ba_id: number;
   boss_dmg: string | null;
@@ -18,10 +22,44 @@ interface BA_Props {
 
 export const BASingle: React.FC<BA_Props> = ({ BAInfo }) => {
   return (
-    <h1>
+    <div className={styles.baSingleContainer}>
       {BAInfo.map((ba, idx) => (
-        <p key={idx}>{ba.damage}</p>
+        <div key={idx} className={styles.singleBAContainer}>
+          <div className={styles.BAHeader}>
+            <p>
+              {ba.player_name} <span>({ba.player_job})</span>
+            </p>
+            <div className={styles.timer}>
+              <FontAwesomeIcon
+                icon={faStopwatch}
+                size="lg"
+                style={{ color: "#e8e8e8" }}
+              />
+              <p>{ba.time}</p>
+            </div>
+          </div>
+          <table>
+            <thead>
+              <tr>
+                <th>Damage</th>
+                <th>IED</th>
+                <th>Boss%</th>
+                <th>Main Stat</th>
+                <th>Range</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>{ba.damage ? ba.damage : "N/A"}</td>
+                <td>{ba.ied ? ba.ied : "N/A"}</td>
+                <td>{ba.boss_dmg ? ba.boss_dmg : "N/A"}</td>
+                <td>{ba.main_stat ? ba.main_stat : "N/A"}</td>
+                <td>{ba.range ? ba.range : "N/A"}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       ))}
-    </h1>
+    </div>
   );
 };
--- pages/ba-history.tsx
+++ pages/ba-history.tsx
@@ -1,13 +1,10 @@
 import { BASingle } from "@/comps/BASingle";
 import SideMenu from "@/comps/SideMenu";
 import styles from "@/styles/BAHistory.module.css";
-import { useSupabaseClient } from "@supabase/auth-helpers-react";
 import Image from "next/image";
 import { useEffect, useState, useCallback } from "react";
 import { supabase } from "../lib/supabaseClient";
 import { Database } from "../types/supabase";
-
-type BAarg = Database["public"]["Tables"]["ba_history"]["Row"];
 
 type BA = Database["public"]["Tables"]["ba_history"]["Row"][];
 
@@ -37,7 +34,6 @@ export default function BAHistory() {
   const [BAInfo, setBAInfo] = useState<BA_Single[] | []>([]);
   console.log(dateList);
   const handleDateClick = (BAinfo: BA_History) => {
-    console.log(BAinfo);
     if (BAinfo.ba) {
       setBAInfo(BAinfo.ba);
     }
@@ -59,10 +55,7 @@ export default function BAHistory() {
 
   useEffect(() => {
     getData();
-    //   need some kind of setter method to set the date selected
-    //   or we can add an onclick, or make them buttons? for the dates
   }, [getData]);
-  console.log("BAINFO", BAInfo);
 
   return (
     <div className={styles.container}>
